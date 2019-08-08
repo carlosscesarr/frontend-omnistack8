0fb9dca78b38af911b69a73f6ac93a95aa7198ed
Carlos <ce-sarr@hotmail.com>
2019-08-07 21:46:43 -0300

    Login e listagem de devs em que o usuário online não possui like ou dislike

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
+import api from '../services/api';
+
 import './Login.css';
 
 import logo from '../assets/logo.svg'
 
-export default function Login() {
+export default function Login({ history }) {
     const [username, setUsername] = useState('');
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
-        console.log(username);
+
+        const response = await api.post('/devs', {
+            username,
+        });
+        const { _id } = response.data;
+
+        history.push(`/dev/${_id}`);
     }
     return (
         <div className="login-container">
             <form onSubmit={handleSubmit}>
-                <img src={logo} alt="Tindev"/>
-                <input 
+                <img src={logo} alt="Tindev" />
+                <input
                     placeholder="Digite seu usuário do Github"
-                    value={username} 
+                    value={username}
                     onChange={e => setUsername(e.target.value)}
                 />
                 <button type="submit">Enviar</button>
