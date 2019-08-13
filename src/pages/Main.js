6549f1b2e949f7bfceac7e13e229b6884468a8bf
Carlos <ce-sarr@hotmail.com>
2019-08-12 22:40:08 -0300

    Função de Match em tempo real  finalizado

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
+import './Main.css';
 
 import api from '../services/api';
-
-import './Main.css';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
-
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }) {
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadUsers() {
@@ -24,6 +25,16 @@ export default function Main({ match }) {
             setUsers(response.data);
         }
         loadUsers();
+    }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        });
+
+        socket.on('match', dev => {
+           setMatchDev(dev);
+        })
     }, [match.params.id]);
 
     async function handleDislike(id) {
@@ -77,7 +88,15 @@ export default function Main({ match }) {
                     <div className="empty">Acabou :(</div>
                 )}
 
-
+            {matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a match" />
+                    <img className="avatar" src={matchDev.avatar} alt="It's a match" />
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+                    <button onClick={() => setMatchDev(null)} type='button'>FECHAR</button>
+                </div>
+            )}
         </div>
     )
 }
